0e5c9cae0eef2461a65ecd969b193b90c9df7912
Jose Ureta <jose.ureta@onroadts.com>
2025-01-05 19:40:15 -0500

    Se optimizo el proceso de los eventos

--- lib/presentation/blocs/04-guests/guests_bloc.dart
+++ lib/presentation/blocs/04-guests/guests_bloc.dart
@@ -10,38 +10,38 @@ const uuid = Uuid();
 
 class GuestsBloc extends Bloc<GuestsEvent, GuestsState> {
   GuestsBloc() : super(const GuestsState()) {
-    on<SetAllFilterEvent>((event, emit) {
-      emit(state.copyWith(filter: GuestsFilter.all));
-    });
-
-    on<SetInvitedFilterEvent>((event, emit) {
-      emit(state.copyWith(filter: GuestsFilter.invited));
-    });
-
-    on<SetNoInvitedFilterEvent>((event, emit) {
-      emit(state.copyWith(filter: GuestsFilter.noInvited));
-    });
-
-    // on<SetCustomFilterEvent>((event, emit) {
-    //   emit(state.copyWith(filter: event.newFilter));
+    // on<SetAllFilterEvent>((event, emit) {
+    //   emit(state.copyWith(filter: GuestsFilter.all));
     // });
+
+    // on<SetInvitedFilterEvent>((event, emit) {
+    //   emit(state.copyWith(filter: GuestsFilter.invited));
+    // });
+
+    // on<SetNoInvitedFilterEvent>((event, emit) {
+    //   emit(state.copyWith(filter: GuestsFilter.noInvited));
+    // });
+
+    on<SetCustomFilterEvent>((event, emit) {
+      emit(state.copyWith(filter: event.newFilter));
+    });
   }
 
-  // void changeFilter(GuestsFilter value) {
-  //   add(SetCustomFilterEvent(value));
+  void changeFilter(GuestsFilter value) {
+    add(SetCustomFilterEvent(value));
+  }
+
+  // void changeFilter(GuestsFilter newFilter) {
+  //   switch (newFilter) {
+  //     case GuestsFilter.all:
+  //       add(SetAllFilterEvent());
+  //       break;
+  //     case GuestsFilter.invited:
+  //       add(SetInvitedFilterEvent());
+  //       break;
+  //     case GuestsFilter.noInvited:
+  //       add(SetNoInvitedFilterEvent());
+  //       break;
+  //   }
   // }
-
-  void changeFilter(GuestsFilter newFilter) {
-    switch (newFilter) {
-      case GuestsFilter.all:
-        add(SetAllFilterEvent());
-        break;
-      case GuestsFilter.invited:
-        add(SetInvitedFilterEvent());
-        break;
-      case GuestsFilter.noInvited:
-        add(SetNoInvitedFilterEvent());
-        break;
-    }
-  }
 }
